9a7d4ddcc2fe4bc239332c4b70e88fd5e17767c0
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-08-16 12:32:14 -0600

    Parse our other wikipedia titles from HTML

--- wikimedia/wikimedia.go
+++ wikimedia/wikimedia.go
@@ -1,17 +1,31 @@
 package wikimedia
 
 import (
-	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func GetPageHTML(title string) (string, error) {
+func GetPageLinks(title string) ([]string, error) {
+	resp, err := getPageHTML(title)
+	if err != nil {
+		return []string{}, err
+	}
+
+	links, err := getPageLinksFromHTMLResponse(resp)
+	defer resp.Body.Close() // No resource leaks please
+
+	return links, nil
+}
+
+// getPageHTML queries the wikimedia api for a title and gets the HTML back
+func getPageHTML(title string) (*http.Response, error) {
 	req, err := GetPageHTMLRoute(title)
 
 	// Check that we didn't have an error getting the page
 	if err != nil {
-		fmt.Print(err)
+		return nil, err
 	}
 
 	// Create our client and send our request
@@ -22,16 +36,54 @@ func GetPageHTML(title string) (string, error) {
 
 	// Check we didn't have an error when we sent our request
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-
-	// Close our request
-	defer resp.Body.Close()
 
 	// Check we got a correct response
 	if resp.StatusCode == 200 { // OK
-		return BodyString(resp)
+		return resp, nil
 	}
 
-	return "", nil //TODO Do something different here.
+	return nil, nil //TODO Do something different here.
+}
+
+// getPageLinksFromHTMLResponse parses the page links from a response
+func getPageLinksFromHTMLResponse(resp *http.Response) ([]string, error) {
+
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return []string{}, err
+	}
+
+	links := parseLinks(doc)
+
+	return links, nil
+}
+
+// inSet is a helper that returns true if the item is in our set
+func inSet(set map[string]bool, item string) bool {
+	_, ok := set[item]
+	return ok
+}
+
+// parseLinks gives the links from a goquery document (the HTML)
+func parseLinks(doc *goquery.Document) []string {
+
+	previouslySeenLinks := map[string]bool{}
+	links := []string{}
+
+	// Find all the links (or "a" tags)
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+
+		href, exists := s.Attr("href")
+		title := LinkToTitle(href)
+
+		weShouldAdd := exists && IsValidLink(href) && !inSet(previouslySeenLinks, title)
+		if weShouldAdd {
+			links = append(links, title)
+			previouslySeenLinks[title] = true
+		}
+	})
+
+	return links
 }
--- wikimedia/wikimedia_test.go
+++ wikimedia/wikimedia_test.go
@@ -1,9 +1,18 @@
 package wikimedia
 
-import "testing"
-import "fmt"
+import (
+	"testing"
 
-func TestGetPageHTML(t *testing.T) {
-	html, _ := GetPageHTML("cats")
-	fmt.Print(html)
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPageLinks(t *testing.T) {
+	links, err := GetPageLinks("cats")
+	assert.Nil(t, err)
+
+	// These tests technically have the posibility of failing.
+	// Since it's possible for someone to edit the wikipedia page.
+	assert.Contains(t, links, "Animal")
+	assert.Contains(t, links, "Felis")
+	assert.Contains(t, links, "Mammal")
 }
